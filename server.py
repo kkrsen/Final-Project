b1131fbba066cf24008c7092402fd24629ccf00d
Kora Sen <korasen@d0-88-0c-7c-b7-4f.wireless.temple.edu>
2024-04-27 17:11:59 -0400

    Website Pages Setup

--- server.py
+++ server.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python
 
 
-#from lib import hello_world
+# from lib import hello_world
 
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+STEPS = {
+        "url_upload": "URL Upload",
+        "select": "Select Date and Line/Station",
+        "timetable": "Timetable",
+    }
+
 @app.route("/")
 def index():
-    moutains = ["Everest", "K2", "Kilimanjaro"]
-    return render_template("index.html", mountain = moutains)
+    return render_template("index.html", steps=STEPS)
 
-@app.route("/mountain/<mt>")
-def mountain(mt):
-    return "This is " + str(mt)
+
+@app.route("/step/url_upload")
+def url_upload():
+    return "This is where you upload your GTFS URL"
+
+@app.route("/step/select")
+def select():
+    return "This is Select Date and Line/Station"
+
+@app.route("/step/timetable")
+def timetable():
+    return "This is Timetable"
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 5000)
+    app.run(host="0.0.0.0", port=5000)
 
 
-
-
-#hello_world()
-
+# hello_world()
