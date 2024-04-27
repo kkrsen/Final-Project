5284354f193ee50468d1060d4ee611a8823f35ba
Kora Sen <korasen@d0-88-0c-7c-b7-4f.wireless.temple.edu>
2024-04-27 17:14:33 -0400

    Got rid of URL Upload Page

--- server.py
+++ server.py
@@ -8,27 +8,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 STEPS = {
-        "url_upload": "URL Upload",
-        "select": "Select Date and Line/Station",
-        "timetable": "Timetable",
-    }
+    "select": "Select Date and Line/Station",
+    "timetable": "Timetable",
+}
+
 
 @app.route("/")
 def index():
     return render_template("index.html", steps=STEPS)
 
 
-@app.route("/step/url_upload")
-def url_upload():
-    return "This is where you upload your GTFS URL"
-
 @app.route("/step/select")
 def select():
     return "This is Select Date and Line/Station"
 
+
 @app.route("/step/timetable")
 def timetable():
     return "This is Timetable"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
