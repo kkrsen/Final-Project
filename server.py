1ddfca8a75894e52bbe8efe218b21ae07673028e
Kora Sen <korasen@d0-88-0c-7c-b7-4f.wireless.temple.edu>
2024-04-27 15:02:32 -0400

    Make website

--- server.py
+++ server.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python
 
 
-from lib import hello_world
+#from lib import hello_world
 
-hello_world()
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    moutains = ["Everest", "K2", "Kilimanjaro"]
+    return render_template("index.html", mountain = moutains)
+
+@app.route("/mountain/<mt>")
+def mountain(mt):
+    return "This is " + str(mt)
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0", port = 5000)
+
+
+
+
+#hello_world()
+
