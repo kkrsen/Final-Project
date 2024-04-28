8b4538c0ad7a2cc0b034963ddab42db1c326e9e1
Kora Sen <korasen@d0-88-0c-7c-b7-4f.wireless.temple.edu>
2024-04-27 23:38:33 -0400

    Make stops and routes into dicts in lists

--- lib.py
+++ lib.py
@@ -3,14 +3,11 @@ from gtfs_kit.feed import read_feed
 
 
 def download_gtfs_file(url: str):
-    print(url)
-    print(type(url))
     feed = read_feed(url, dist_units="mi")
-    df_stops = pd.DataFrame(feed.stops)
-    df_stops = df_stops.groupby("stop_name").first().reset_index()
-    stops = df_stops["stop_name"].tolist()
-    print(stops)
-    df_routes = pd.DataFrame(feed.routes)
-    routes = df_routes["route_id"].tolist()
-    print(routes)
+    stop_id = feed.stops["stop_id"]
+    # filter out North/South duplicates
+    stops = feed.stops[~stop_id.str.endswith("N")][~stop_id.str.endswith("S")].to_dict(
+        orient="records"
+    )
+    routes = feed.routes.to_dict(orient="records")
     return stops, routes
